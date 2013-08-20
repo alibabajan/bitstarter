aef2ebc5ec555cf30d17f1fdc8092f6d7a7efcd1
Alireza <ithoughtful@gmail.com>
2013-08-20 21:01:58 +0200

    transform index make responsive

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
   response.send(readFile());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8888;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
